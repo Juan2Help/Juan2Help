07e42946efec1ac3aaf8806acb02cec672288eca
Jarred Luzada <jarredluzada@gmail.com>
2022-03-01 23:36:35 +0800

    Added Signup Page.

---
+++ components/Input.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Input({
+    id,
+    name,
+    type,
+    autoComplete,
+    required,
+    placeholder,
+    onChange,
+}) {
+    return (
+        <div>
+            <label htmlFor={name} className="sr-only">
+                {placeholder}
+            </label>
+            <input
+                id={id}
+                name={name}
+                type={type}
+                autoComplete={autoComplete}
+                required={required}
+                className="appearance-none relative block w-full px-4 py-3 border border-gray-300 placeholder-gray-400 text-gray-900 font-medium rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder={placeholder}
+                onChange={onChange}
+            />
+        </div>
+    );
+}
+
+export default Input;
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,176 +1,182 @@
-import { useState } from "react";
+import { useState } from 'react';
 import {
-  getProviders,
-  signIn as signIntoProviders,
-  useSession,
-  signOut,
-} from "next-auth/react";
-import Button from "../../components/Button";
+    getProviders,
+    signIn as signIntoProviders,
+    useSession,
+    signOut,
+} from 'next-auth/react';
+import Head from 'next/head';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 function signin({ providers }) {
-  //get session
-  const { data: session } = useSession();
+    //get session
+    const { data: session } = useSession();
 
-  const [userDetails, setUserDetails] = useState({ email: "", password: "" });
-  const { email, password } = userDetails;
+    const [userDetails, setUserDetails] = useState({ email: '', password: '' });
+    const { email, password } = userDetails;
 
-  console.log("providers", providers);
-  console.log("session", session);
+    console.log('providers', providers);
+    console.log('session', session);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    //check if fields are empty
-    if (!email || !password) {
-      alert("Please enter email and password");
-      return;
+        //check if fields are empty
+        if (!email || !password) {
+            alert('Please enter email and password');
+            return;
+        }
+
+        // try to sign in
+        try {
+            const result = await signIntoProviders('credentials', {
+                redirect: false,
+                email: email,
+                password: password,
+            });
+
+            console.log(session);
+        } catch (err) {
+            console.error('Error: ', err);
+        }
+
+        return;
+    };
+
+    const handleChange = (e) => {
+        // Grab values from form and create local state
+        const { name, value } = e.target;
+
+        setUserDetails({ ...userDetails, [name]: value });
+        console.log(name, value);
+        console.log('userDetails', userDetails);
+    };
+
+    if (session) {
+        return (
+            <>
+                <Head>
+                    <title>Welcome back {session.user.name}!</title>
+                </Head>
+                <div className="min-h-screen flex items-center justify-center">
+                    <div className="min-h-full py-12 px-4 sm:px-6 lg:px-8">
+                        <div className="max-w-md w-full space-y-8">
+                            <div>
+                                You are already signed in as {session.user.name}
+                                !
+                            </div>
+                            <Button text="Signout" onClick={() => signOut()} />
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
     }
 
-    // try to sign in
-    try {
-      const result = await signIntoProviders("credentials", {
-        redirect: false,
-        email: email,
-        password: password,
-      });
-
-      console.log(session);
-    } catch (err) {
-      console.error("Error: ", err);
-    }
-
-    return;
-  };
-
-  const handleChange = (e) => {
-    // Grab values from form and create local state
-    const { name, value } = e.target;
-
-    setUserDetails({ ...userDetails, [name]: value });
-    console.log(name, value);
-    console.log("userDetails", userDetails);
-  };
-
-  if (session) {
     return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="min-h-full py-12 px-4 sm:px-6 lg:px-8">
-          <div className="max-w-md w-full space-y-8">
-            <div>You are already signed in as {session.user.name}!</div>
-            <Button text="Signout" onClick={() => signOut()} />
-          </div>
-        </div>
-      </div>
-    );
-  }
+        <>
+            <Head>
+                <title>Sign In</title>
+            </Head>
+            <div className="min-h-screen flex items-center justify-center">
+                <div className="min-h-full py-12 px-4 sm:px-6 lg:px-8">
+                    <div className="max-w-md w-full space-y-4">
+                        <div>
+                            <img
+                                className="mx-auto h-12 w-auto"
+                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                alt="Workflow"
+                            />
+                            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                                Sign in to your account
+                            </h2>
+                        </div>
+                        <form
+                            className="mt-8 space-y-6"
+                            action="#"
+                            method="POST"
+                            onSubmit={handleSubmit}
+                        >
+                            <input type="hidden" name="remember" value="true" />
+                            <div className="rounded-md shadow-sm -space-y-px">
+                                <Input
+                                    id="email-address"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    placeholder="Email address"
+                                    onChange={handleChange}
+                                />
+                                <Input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    placeholder="Password"
+                                    onChange={handleChange}
+                                />
+                            </div>
 
-  return (
-    <>
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="min-h-full py-12 px-4 sm:px-6 lg:px-8">
-          <div className="max-w-md w-full space-y-8">
-            <div>
-              <img
-                className="mx-auto h-12 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                alt="Workflow"
-              />
-              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                Sign in to your account
-              </h2>
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-center">
+                                    <input
+                                        id="remember-me"
+                                        name="remember-me"
+                                        type="checkbox"
+                                        className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                                    />
+                                    <label
+                                        htmlFor="remember-me"
+                                        className="ml-2 block text-sm text-gray-900"
+                                    >
+                                        {' '}
+                                        Remember me{' '}
+                                    </label>
+                                </div>
+
+                                <div className="text-sm">
+                                    <a
+                                        href="#"
+                                        className="font-medium text-indigo-600 hover:text-indigo-500"
+                                    >
+                                        {' '}
+                                        Forgot your password?{' '}
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div>
+                                <Button text="Log In" />
+                            </div>
+                        </form>
+                        <div className="w-full text-sm text-gray-400 font-medium text-center mt-0">
+                            - OR -
+                        </div>
+                        <Button
+                            text="Sign in with Google"
+                            isGoogleSignIn={true}
+                            onClick={() =>
+                                signIntoProviders(providers.google.id)
+                            }
+                        />
+                    </div>
+                </div>
             </div>
-            <form
-              className="mt-8 space-y-6"
-              action="#"
-              method="POST"
-              onSubmit={handleSubmit}
-            >
-              <input type="hidden" name="remember" value="true" />
-              <div className="rounded-md shadow-sm -space-y-px">
-                <div>
-                  <label htmlFor="email-address" className="sr-only">
-                    Email address
-                  </label>
-                  <input
-                    id="email-address"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    placeholder="Email address"
-                    onChange={handleChange}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="password" className="sr-only">
-                    Password
-                  </label>
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    placeholder="Password"
-                    onChange={handleChange}
-                  />
-                </div>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                  />
-                  <label
-                    htmlFor="remember-me"
-                    className="ml-2 block text-sm text-gray-900"
-                  >
-                    {" "}
-                    Remember me{" "}
-                  </label>
-                </div>
-
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    {" "}
-                    Forgot your password?{" "}
-                  </a>
-                </div>
-              </div>
-
-              <div>
-                <Button text="Login" />
-              </div>
-            </form>
-            <Button
-              text="Sign in with Google"
-              isGoogleSignIn={true}
-              onClick={() => signIntoProviders(providers.google.id)}
-            />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 export async function getServerSideProps() {
-  const providers = await getProviders();
+    const providers = await getProviders();
 
-  return {
-    props: {
-      providers,
-    },
-  };
+    return {
+        props: {
+            providers,
+        },
+    };
 }
 
 export default signin;
