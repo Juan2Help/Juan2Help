01378d288ba0c2519b5ae97508cf05348b191725
Joshua Espartero <jmespartero@up.edu.ph>
2022-04-29 15:18:32 +0800

    Sprint 4: Debug 3

--- pages/manage/edit-admin/index.js
+++ pages/manage/edit-admin/index.js
@@ -16,7 +16,7 @@ function edit({ sessionFromProp, organizationDetailsProp }) {
   const session = sessionFromProp;
 
   const [organizationDetails, setOrganizationDetails] = useState({
-    fetchNGODetails,
+    organizationDetailsProp,
   });
   const router = useRouter();
 
@@ -62,7 +62,7 @@ function edit({ sessionFromProp, organizationDetailsProp }) {
       <div className="bg-white min-h-screen w-screen px-4 flex flex-col">
         <div className="bg-white sticky top-0 text-xl py-4 z-50 flex flex-row w-full items-center space-x-2">
           <Link href="/manage">
-            <FiArrowLeft className="cursor-pointer hover:text-gray-500"/>
+            <FiArrowLeft className="cursor-pointer hover:text-gray-500" />
           </Link>
           <span className="font-bold">Edit Partner Organization Details</span>
         </div>
